4f26b9e05a200d3df216d8251d02a1c227196fcc
billu <billu@WMUREN-NB2>
2022-11-21 00:15:22 +0800

    add schoolservice codes

--- MVCWebNTier/Service/ISchoolService.cs
+++ MVCWebNTier/Service/ISchoolService.cs
@@ -1,6 +1,18 @@
-﻿namespace Service
+﻿using MVCWebNTier.Models;
+using System;
+
+namespace Service
 {
     public interface ISchoolService
     {
+        Task<IEnumerable<School>> GetAllPeople();
+        Task<int> AddSchool(School school);
+
+        Task<School> GetSchoolByID(string id);
+
+        Task<int> CreateSchool(School school);
+        Task<int> UpdateSchool(School school);
+        Task<School> FindSchoolById(string id);
+        bool DeleteSchool(School school);
     }
 }
--- MVCWebNTier/Service/SchoolService.cs
+++ MVCWebNTier/Service/SchoolService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MVCWebNTier.Models;
+using Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,62 @@ namespace Service
 {
     public class SchoolService : ISchoolService
     {
+        ISchoolRepository _schoolRepository;
+
+        public SchoolService(ISchoolRepository schoolRepository)
+        {
+            _schoolRepository = schoolRepository;
+        }
+
+        public async Task<List<School>> AddSchool(School school)
+        {
+            return await _schoolRepository.AddSchool(school);
+        }
+
+        public Task<int> CreateSchool(School School)
+        {
+            return _schoolRepository.CreateSchool(School);
+        }
+
+        public bool DeleteSchool(School School)
+        {
+            return _schoolRepository.DeleteSchool(School);
+        }
+
+        public async Task<School> FindSchoolById(string id)
+        {
+            return await _schoolRepository.FindSchoolById(id);
+        }
+
+        public async Task<IEnumerable<School>> GetAllSchools()
+        {
+            return await _schoolRepository.GetAllSchools();
+        }
+
+        public School GetSchool(string id)
+        {
+            var newSchool = new School()
+            {
+                SchoolName = "Nicole",
+                SchoolID = "Kidman"
+            };
+
+            return newSchool;
+        }
+
+        public async Task<School> GetSchoolByID(string id)
+        {
+            return await _schoolRepository.GetSchoolById(id);
+        }
+
+        public async Task<int> UpdateSchool(School school)
+        {
+            return await _schoolRepository.UpdateSchool(school);
+        }
+
+        public async Task<int> AddSchool(School school)
+        {
+            return await _schoolRepository.AddSchool(school);
+        }
     }
 }
