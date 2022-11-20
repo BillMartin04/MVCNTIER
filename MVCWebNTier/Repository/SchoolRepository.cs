6072c6a09ad41d5c08c65d023c0e4eb01c7cd11b
billu <billu@WMUREN-NB2>
2022-11-20 22:34:28 +0800

    Updated repository

--- MVCWebNTier/Repository/SchoolRepository.cs
+++ MVCWebNTier/Repository/SchoolRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MVCWebNTier.Data.Context;
+using MVCWebNTier.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,68 @@ namespace Repository
 {
     public class SchoolRepository : ISchoolRepository
     {
+        private readonly SchoolContext _context;
+        public SchoolRepository(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> AddSchool(School school)
+        {
+            _context.Add(school);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> CreateSchool(School school)
+        {
+            _context.Add(school);
+            return await _context.SaveChangesAsync();
+        }
+
+        public bool DeleteSchool(School school)
+        {
+            _context.Schools.Remove(school);
+            return true;
+        }
+
+        public int EditSchool(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<School> FindSchoolById(string id)
+        {
+            return await _context.Schools.FindAsync(id);
+        }
+
+        public int GetAge(string id)
+        {
+            var School = GetSchoolById(id);
+            throw new NotImplementedException();
+        }
+
+        public int GetAge(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public async Task<IEnumerable<School>> GetAllSchools()
+        {
+            return await _context.Schools.ToListAsync();
+        }
+
+        public async Task<School> GetSchoolById(string id)
+        {
+            return await _context.Schools
+           .FirstOrDefaultAsync(m => m.SchoolID == id);
+        }
+
+        public async Task<int> UpdateSchool(School School)
+        {
+            _context.Update(School);
+            return await _context.SaveChangesAsync();
+        }
     }
 }
